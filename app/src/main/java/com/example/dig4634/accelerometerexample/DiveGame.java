72ec50532132e51b063f8b18a150fb11c070cd99
Julia Chancey <julia@Julias-MacBook-Air-3.local>
2022-10-03 11:48:28 -0400

    added turtle

--- app/src/main/java/com/example/dig4634/accelerometerexample/DiveGame.java
+++ app/src/main/java/com/example/dig4634/accelerometerexample/DiveGame.java
@@ -8,14 +8,17 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import java.util.Random;
 
 public class DiveGame extends AppCompatActivity implements SensorEventListener, SurfaceHolder.Callback {
 
@@ -23,7 +26,10 @@ public class DiveGame extends AppCompatActivity implements SensorEventListener, 
     Paint white_stroke;
     Paint white_text;
     Bitmap diver;
-    Bitmap sun;
+    Bitmap lionfish;
+    Bitmap turtle;
+    Point size;
+    Random rand;
 
     int points = 0;
     String message = "Points: " + points;
@@ -34,6 +40,7 @@ public class DiveGame extends AppCompatActivity implements SensorEventListener, 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        rand = new Random();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dive);
 
@@ -51,8 +58,14 @@ public class DiveGame extends AppCompatActivity implements SensorEventListener, 
         white_text.setTextSize(100);
 
         diver=Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.diver),500,500,false);
-        sun=Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.lionfish),200,200,false);
+        lionfish=Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.lionfish),200,200,false);
+        turtle=Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.turtle),200,200,false);
 
+        Display display = getWindowManager().getDefaultDisplay();
+
+        //Getting the screen resolution into point object
+        size = new Point();
+        display.getSize(size);
 
         SensorManager manager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
         Sensor accelerometer=manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -83,14 +96,18 @@ public class DiveGame extends AppCompatActivity implements SensorEventListener, 
     float acc_x=0;
     float acc_y=0;
     int diver_x_position=310;
-    int diver_y_position=1320;
+    int diver_y_position=1500;
 
-    int sun_x_position=500;
-    int sun_y_position=600;
+    int lionfish_x_position=500;
+    int lionfish_y_position=600;
+
+    int turtle_x_position=200;
+    int turtle_y_position=100;
 
     public void update(int width, int height){
 
-        sun_y_position+=5;
+        lionfish_y_position+=5;
+        turtle_y_position+=5;
 
         diver_x_position-=acc_x*2;
         diver_y_position+=acc_y*2;
@@ -98,38 +115,42 @@ public class DiveGame extends AppCompatActivity implements SensorEventListener, 
         if(diver_x_position<0)diver_x_position=0;
         else if(diver_x_position>width-200)diver_x_position=width-200;
 
-        if (Math.abs(diver_x_position-sun_x_position)<200 && Math.abs(diver_y_position-sun_y_position)<200){
+        if (Math.abs(diver_x_position-lionfish_x_position)<200 && Math.abs(diver_y_position-lionfish_y_position)<200){
             points++;
             message="Points: " + points;
-            sun_x_position = 0;
-            sun_y_position = 0;
+            lionfish_x_position = rand.nextInt(size.x-200);
+            lionfish_y_position = 0;
+        }
+
+        if (Math.abs(diver_x_position-turtle_x_position)<200 && Math.abs(diver_y_position-turtle_y_position)<200){
+            points--;
+            message="Points: " + points;
+            turtle_x_position = rand.nextInt(size.x-200);
+            turtle_y_position = 0;
+        }
+
+        if (turtle_y_position > size.y){
+            turtle_x_position = rand.nextInt(size.x-200);
+            turtle_y_position = 0;
+        }
+
+        if (lionfish_y_position > size.y){
+            lionfish_x_position = rand.nextInt(size.x-200);
+            lionfish_y_position = 0;
         }
 
     }
 
     public void draw(){
-
         if(holder==null)return;
 
-
-
-
         Canvas c=holder.lockCanvas();
-
         update(c.getWidth(),c.getHeight());
 
         c.drawColor(Color.rgb(145,186,214));
-
-        //c.drawRect(0,0, 200,200,red_fill);
-
-        //c.drawCircle(c.getWidth()/2,c.getHeight()/2,100,white_stroke);
-
-
-
-        c.drawBitmap(sun,sun_x_position,sun_y_position,null);
-
+        c.drawBitmap(lionfish,lionfish_x_position,lionfish_y_position,null);
         c.drawBitmap(diver,diver_x_position,diver_y_position,null);
-
+        c.drawBitmap(turtle, turtle_x_position, turtle_y_position, null);
         c.drawText(message, 50, 100, white_text);
 
         holder.unlockCanvasAndPost(c);
